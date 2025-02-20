8a3e41c98b3b31d427142baedd2bf42e95e44eb0
Douglas Souza <douglasmasouza7@gmail.com>
2025-02-20 20:19:00 -0300

    segunda tentativa

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,29 +1,20 @@
-.data
-N:    .word 5       
-RES:  .word 0       
+LV 100      ; Carrega o endereço do número N
+LD 100      ; AC = N (Lê o valor armazenado em 0x100)
+JZ FIM      ; Se N == 0, vai para FIM
+LV 1        ; AC = 1 (Inicializa resultado)
+MM 101      ; Armazena resultado temporário em 0x101 (resultado = 1)
 
-.text
-.globl main
-main:
-    
-    la   $t1, N      
-    lw   $t0, 0($t1) 
+LOOP:
+    LD 101  ; Carrega resultado atual
+    ML 100  ; Multiplica por N (AC = AC * N)
+    MM 101  ; Salva resultado em 0x101
+    LD 100  ; Carrega N
+    SB 1    ; Subtrai 1 (N = N - 1)
+    MM 100  ; Salva N atualizado
+    JN FIM  ; Se N < 0, termina (evita multiplicação errada quando chega a 1)
+    JP LOOP ; Continua o loop
 
-    
-    li   $t2, 1      
-    li   $t3, 1      
-
-loop:
-    ble  $t0, $t3, end   
-    mul  $t2, $t2, $t3   
-    addi $t3, $t3, 1     
-    j    loop            
-
-end:
-    
-    la   $t1, RES
-    sw   $t2, 0($t1)
-
-    
-    li   $v0, 10
-    syscall
+FIM:
+    LD 101  ; Carrega resultado final
+    MM 102  ; Salva no endereço 0x102
+    HM      ; Finaliza o programa
